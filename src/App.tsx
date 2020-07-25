7c4f01e077240e087d4ebb0a34b7f7d4ca5005e3
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-25 17:15:40 -0300

    Routes setup | First route to Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from 'routes';
+import history from 'services/history';
 import GlobalStyle from 'styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <h1>Ola mundo</h1>
+      <Router history={history}>
+        <Routes />
+      </Router>
       <GlobalStyle />
     </>
   );
