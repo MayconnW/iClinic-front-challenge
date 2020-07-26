62cf470597730e22fbe1f727614f9c3adc4fc30c
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-25 22:36:29 -0300

    Create a theme for each side of the force

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,6 @@
+export default {
+  color: {
+    primary: '#2b2b2b',
+    secondary: '#fff',
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,6 @@
+export default {
+  color: {
+    primary: '#FBFE63',
+    secondary: '#2A2A2A',
+  },
+};
