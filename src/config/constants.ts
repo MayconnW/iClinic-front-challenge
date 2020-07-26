e6e90327bb7cc81c1863e66e2d9bcc388555e697
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-25 22:37:43 -0300

    Refactor getPathSide Service

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,4 @@
 export const LIGHT_SIDE = 1;
 export const DARK_SIDE = 4;
+
+export type Side = typeof LIGHT_SIDE | typeof DARK_SIDE;
--- src/services/getPathSide.ts
+++ src/services/getPathSide.ts
@@ -1,17 +1,17 @@
-import { DARK_SIDE, LIGHT_SIDE } from 'config/constants';
+import { DARK_SIDE, LIGHT_SIDE, Side } from 'config/constants';
 import api from './api';
 
-const lightSide = async (): Promise<typeof LIGHT_SIDE> => {
+const lightSide = async (): Promise<Side> => {
   await api.get(`people/${LIGHT_SIDE}`);
   return LIGHT_SIDE;
 };
 
-const darkSide = async (): Promise<typeof DARK_SIDE> => {
+const darkSide = async (): Promise<Side> => {
   await api.get(`people/${DARK_SIDE}`);
   return DARK_SIDE;
 };
 
-export default async (): Promise<typeof DARK_SIDE | typeof LIGHT_SIDE> => {
+export default async (): Promise<Side> => {
   const response = await Promise.race([lightSide(), darkSide()]);
   return response;
 };
