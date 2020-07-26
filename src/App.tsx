317de6769ac49ea4f96410f5b5c094a060887921
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-07-26 19:18:25 -0300

    Reading default theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import { Router } from 'react-router-dom';
 import Routes from 'routes';
 import history from 'services/history';
 import GlobalStyle from 'styles/global';
+import { ThemeContext } from 'styled-components';
+import { defaultTheme } from 'styles/themes';
 
 dotenv.config();
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeContext.Provider value={defaultTheme}>
       <Router history={history}>
         <Routes />
       </Router>
       <GlobalStyle />
-    </>
+    </ThemeContext.Provider>
   );
 };
 
